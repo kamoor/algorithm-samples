1f4156fcbc061b5a00703c29f2084a62d99405d1
kamoorr <rkamoor@gmail.com>
2014-12-11 22:59:30 -0500

    recursive and non recursive

--- src/main/java/com/test/numbertheory/GcdEuclideanWay.java
+++ src/main/java/com/test/numbertheory/GcdEuclideanWay.java
@@ -10,7 +10,7 @@ public class GcdEuclideanWay {
 
 	
 	/**
-	 * 
+	 * Eclidian non recursive way
 	 * @param x
 	 * @param y
 	 */
@@ -28,9 +28,28 @@ public class GcdEuclideanWay {
 			divisor = remainder;
 		}
 	}
+	
+	/**
+	 * Recursive way of implementing euclidian algorithm
+	 * @param x
+	 * @param y
+	 */
+	private int findGcdRecursive(int x, int y){
+		
+			if(y == 0){
+				return x;
+			}else{
+				return findGcdRecursive(y, x%y);
+			}
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println( new GcdEuclideanWay().findGcd(1785,  546));
 		System.out.println( new GcdEuclideanWay().findGcd(99,  78));
+		System.out.println( new GcdEuclideanWay().findGcd(30,  24));
+		
+		System.out.println( new GcdEuclideanWay().findGcdRecursive(1785,  546));
 	}
 
 }
