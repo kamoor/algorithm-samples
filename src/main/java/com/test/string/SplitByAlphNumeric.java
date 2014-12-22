511088655d20f9cb6cbe9cf812eb21b09debe401
kamoorr <rkamoor@gmail.com>
2014-12-21 21:43:33 -0500

    regex split

---
+++ src/main/java/com/test/string/SplitByAlphNumeric.java
@@ -0,0 +1,17 @@
+package com.test.string;
+
+public class SplitByAlphNumeric {
+	
+	static String regEx = "[^A-Z0-9]+|(?<=[A-Z])(?=[0-9])|(?<=[0-9])(?=[A-Z])";
+
+	public static void main(String[] args) {
+		String s ="CC23QQ21HD32";
+		String[] splits = s.split(regEx);
+		for(String chunk: splits){
+			System.out.println(chunk);
+		}
+		
+
+	}
+
+}
